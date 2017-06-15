0fbb6870446abc62fa3b657c85a9d6ba196dc2b0
Asan Ozis <symbyozis@gmailc.com>
2017-06-15 17:59:56 +0300

    fix player

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,14 +1,59 @@
-var player;
+var player, audio, curIndex = 0;
+var isPlaying = $('#play').is('.hide');
+var mp3 = [
+'https://verses.quran.com/Alafasy/mp3/001006.mp3',
+'https://verses.quran.com/Alafasy/mp3/001007.mp3',
+'https://verses.quran.com/Alafasy/mp3/001008.mp3',
+'https://verses.quran.com/Alafasy/mp3/001009.mp3',
+];
 
 $(document).ready(function() {
 
-  audiojs.events.ready(function() {
-    player = audiojs.create($('audio')[0]);
-  });
+	audio = $('audio')[0];
+
+	audiojs.events.ready(function() {
+		player = audiojs.create(audio);
+	});
 
 });
 
+function playPause() {
 
-function play() {
+	if (isPlaying) {
+		$('#play').removeClass('hide');
+		$('#pause').addClass('hide');
+		player.pause();
+		isPlaying = false;
+	}
+	else {
+		$('#pause').removeClass('hide');
+		$('#play').addClass('hide');
+		player.play();
+		isPlaying = true;
+	}
+}
+
+
+function next() {
+	curIndex++;
+	player.load(mp3[curIndex]);
 	player.play();
+	if (!isPlaying){
+		$('#pause').removeClass('hide');
+		$('#play').addClass('hide');
+	}
+}
+
+function down() {
+	curIndex--;
+	player.load(mp3[curIndex]);
+	player.play();
+	if (!isPlaying){
+		$('#pause').removeClass('hide');
+		$('#play').addClass('hide');
+	}
+}
+
+function mute() {
+	audio.muted = false;
 }
