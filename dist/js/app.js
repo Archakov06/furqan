5eba3b2244f343ee24ed19d8a24c603f297a5b9f
saidmislaur <saidmislaur@gmail.com>
2017-06-21 16:11:14 +0300

    второй блок

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,10 +8,18 @@
 
 
 $(document).ready(function(){
-   
+
    $('.welcome-block__hamburger').click(function(){
        $(this).toggleClass('welcome-block__hamburger--active');
-       $('.welcome-block__menu').toggleClass('welcome-block__menu--show')
+       $('.welcome-block__side-menu--left').toggleClass('welcome-block__side-menu--left-show');
+     
    });
+
+    $('.welcome-block__language').click(function(){
+        $(this).toggleClass('welcome-block__language--active');
+        $('.welcome-block__side-menu--right').toggleClass('welcome-block__side-menu--right-show');
+    }); 
+
+    $('.welcome-block__logo-big').addClass('fadeInUp');
    
 });
