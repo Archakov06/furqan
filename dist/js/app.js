94e12b09cf63783bb19f67c5ca97f366152b7e52
saidmislaur <saidmislaur@gmail.com>
2017-07-03 18:00:36 +0300

    list

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,26 +10,26 @@
 $(document).ready(function(){
 
     function openMenu(side) {
-        $('.welcome-block__side-menu--' + side).toggleClass('welcome-block__side-menu--' + side + '-show');
+        $('.side-menu--' + side).toggleClass('side-menu--' + side + '-show');
     }
 
-   $('.welcome-block__hamburger').click(function(){
-       $(this).toggleClass('welcome-block__hamburger--active');
+   $('.header__hamburger').click(function(){
+       $(this).toggleClass('header__hamburger--active');
        openMenu('left');
    });
 
-    $('.welcome-block__language, .welcome-block__side-menu--right h3').click(function(){
+    $('.header__language, .side-menu--right h3').click(function(){
         openMenu('right');
     }); 
 
     $('.welcome-block__logo-big').addClass('fadeInUp');
 
-    $('.welcome-block__side-menu--right a').click(function(){
+    $('.side-menu--right a').click(function(){
         var language = $(this).text();
         $('span').text(language);
         openMenu('right');
         var a = $(this).attr('data-language');
-        alert(a);
+        
    });
 
    
